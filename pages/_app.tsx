88868c71c58e4b9ff1b3a0b52c19d229659e2e45
Jesus <lilseniorj@jesusvarguer18@gmail.com>
2023-05-15 23:46:28 -0500

    Auth UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Layout from '@/components/Layout'
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
+import '@/styles/globals.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
