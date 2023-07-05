53a9eb6816e347c97247e4ec5b29e3194010fa1d
Jesus <lilseniorj@jesusvarguer18@gmail.com>
2023-07-04 19:00:35 -0500

    Users and Individual User Profile

---
+++ components/Avatar.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+import useUser from "@/hooks/useUser";
+import Image from "next/image";
+
+interface AvatarProps {
+  userId: string,
+  isLarge?: boolean,
+  hasBorder?: boolean,
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+  userId,
+  isLarge,
+  hasBorder
+}) => {
+  const router = useRouter();
+  const { data: fetchedUser} = useUser(userId);
+
+  const onClick = useCallback((event: any) => {
+    event.stopPropagation();
+
+    const url = `/users/${userId}`;
+
+    router.push(url);
+  }, [router, userId]);
+
+  return(
+    <div
+      className={`
+        ${hasBorder ? 'border-4 border-black' : ''}
+        ${isLarge ? 'h-12' : 'h-12'}
+        ${isLarge ? 'w-12' : 'w-12'}
+        rounded-full
+        hover:opacity-90
+        transition
+        cursor-pointer
+        relative
+      `}
+    >
+      <Image
+        fill
+        style={{
+          objectFit: 'cover',
+          borderRadius: '100%'
+        }}
+        alt="Avatar"
+        onClick={onClick}
+        src={fetchedUser?.profileImage || '/images/placeholder.png'}
+      />
+    </div>
+  );
+}
+
+export default Avatar;
