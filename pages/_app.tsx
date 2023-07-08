e76408a57f9d7abdf60e0fb364bd937687258f02
Jesus <lilseniorj@jesusvarguer18@gmail.com>
2023-07-08 00:26:31 -0500

    Edit User, Image Upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast'
-import { SessionProvider } from 'next-auth/react'
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
 import Layout from '@/components/Layout'
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
 import '@/styles/globals.css'
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Toaster />
       <RegisterModal />
       <LoginModal />
+      <EditModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
