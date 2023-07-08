07a498cbc901e318e9bc3d80388e3ce0fb625153
Jesus <lilseniorj@jesusvarguer18@gmail.com>
2023-07-08 00:25:51 -0500

    Edit User, Image Upload

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -5,6 +5,8 @@ import { BiCalendar } from "react-icons/bi";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 
+import useEditModal from "@/hooks/useEditModal";
+
 import Button from "../Button";
 
 interface UserBioProps {
@@ -14,6 +16,8 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: currentUser } = useCurrentUser();
   const { data: fetchedUser } = useUser(userId);
+
+  const editModal = useEditModal();
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
@@ -27,7 +31,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? (
-          <Button secondary label="Edit" onClick={() => {}} />
+          <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
           <Button
             onClick={() =>{}}
