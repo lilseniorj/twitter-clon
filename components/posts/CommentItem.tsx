449eb15ef5565786b6dd67cb8ebde371a9ce34c8
Jesus <lilseniorj@jesusvarguer18@gmail.com>
2023-07-19 18:55:40 -0500

    Comments Upload

--- components/posts/CommentItem.tsx
+++ components/posts/CommentItem.tsx
@@ -1,32 +1,31 @@
-import { formatDistanceToNowStrict } from "date-fns";
-import { useRouter } from "next/router";
-import { useCallback, useMemo } from "react";
+import { useRouter } from 'next/router';
+import { useCallback, useMemo } from 'react';
+import { formatDistanceToNowStrict } from 'date-fns';
 
-import Avatar from "../Avatar";
-
+import Avatar from '../Avatar';
 
 interface CommentItemProps {
-  data: Record<string, any>
+  data: Record<string, any>;
 }
 
-const CommentItem: React.FC<CommentItemProps> = ({ data }) => {
+const CommentItem: React.FC<CommentItemProps> = ({ data = {} }) => {
   const router = useRouter();
 
-  const goToUser = useCallback((event: any) => {
-    event.stopPropagation()
+  const goToUser = useCallback((ev: any) => {
+    ev.stopPropagation();
 
-    router.push(`/users/${data.user.id}`);
+    router.push(`/users/${data.user.id}`)
   }, [router, data.user.id]);
 
-  const createdAt =  useMemo(() => {
+  const createdAt = useMemo(() => {
     if (!data?.createdAt) {
       return null;
     }
 
-    return formatDistanceToNowStrict(new Date(data.createdAt))
-  }, [data?.createdAt]);
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data.createdAt])
 
-  return(
+  return (
     <div
       className="
         border-b-[1px]
@@ -35,8 +34,7 @@ const CommentItem: React.FC<CommentItemProps> = ({ data }) => {
         cursor-pointer
         hover:bg-neutral-900
         transition
-      "
-    >
+      ">
       <div className="flex flex-row items-start gap-3">
         <Avatar userId={data.user.id} />
         <div>
@@ -44,14 +42,15 @@ const CommentItem: React.FC<CommentItemProps> = ({ data }) => {
             <p
               onClick={goToUser}
               className="
-              text-white
-              font-semibold
-              cursor-pointer
-              hover:underline
+                text-white
+                font-semibold
+                cursor-pointer
+                hover:underline
             ">
               {data.user.name}
             </p>
             <span
+              onClick={goToUser}
               className="
                 text-neutral-500
                 cursor-pointer
