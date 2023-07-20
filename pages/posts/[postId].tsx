53af20c3bd4732e459967f8b31cde397917d4673
Jesus <lilseniorj@jesusvarguer18@gmail.com>
2023-07-19 19:13:59 -0500

    Comments

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
 
 import usePost from "@/hooks/usePost";
@@ -15,7 +15,7 @@ const PostView = () => {
   const { data: fetchedPost, isLoading } = usePost(postId as string);
 
   if (isLoading || !fetchedPost) {
-    return(
+    return (
       <div className="flex justify-center items-center h-full">
         <ClipLoader color="lightblue" size={80} />
       </div>
@@ -24,17 +24,12 @@ const PostView = () => {
 
   return(
     <>
-      <Header label="Tweet" showBackArrow />
+      <Header showBackArrow label="Tweet" />
       <PostItem data={fetchedPost} />
-      <Form
-        postId={postId as string}
-        isComment
-        placeholder="Tweet your reply"
-      />
+      <Form postId={postId as string} isComment placeholder="Tweet your reply" />
       <CommentFeed comments={fetchedPost?.comments} />
     </>
   );
 }
-
 
 export default PostView;
